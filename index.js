c46e0ef2f3cc3b56269cbbe524bda74cc1f099c6
Sach <jayaweerasachin@gmail.com>
2019-08-13 14:43:28 -0400

    InfoManager can now take data from a text database using json: using this it allows the user to select a catagory. Within the catagory you can select a topic and it will display info about that topic.

--- index.js
+++ index.js
@@ -1,32 +1,99 @@
 var currentPage = null;
 var currentCatagory = null;
+var fileData = getData();
 
-function displayCatagory(catagoryName){
-  if(currentCatagory != catagoryName){
-    clearCurrentPage("catagories", "currentCatagory");
-    printCatagory(catagoryName);
+function displayCatagories(){
+  clearCurrentPage("everything", "currentCatagory");
+
+  currentPage = null;
+
+  if(document.getElementById("currentPage") != null){
+    clearCurrentPage("info", "currentPage");
   }
-  currentCatagory = catagoryName;
+
+  var currentCatagory = document.createElement("div");
+  currentCatagory.id = "currentCatagory";
+  document.getElementById("everything").appendChild(currentCatagory);
+
+  for(var i = 0; i < fileData.length; i++){
+    createCatagoryButton(fileData[i]);
+  }
 }
 
-function displayCharacter(characterName){
-  if(currentPage != characterName){
+function displayTopics(catagory){
+  clearCurrentPage("everything", "currentCatagory");
+
+  numberOfTopics = catagory.topic.length;
+  
+  var currentCatagory = document.createElement("div");
+  currentCatagory.id = "currentCatagory";
+  document.getElementById("everything").appendChild(currentCatagory);
+
+  for(var i = 0; i < numberOfTopics; i++){
+    createTopicButton(catagory.topic[i]);
+  }
+
+  createBackButton();
+}
+
+function createBackButton(){
+  var backButton = document.createElement("input");
+  backButton.type = "button";
+  backButton.value = "Back";
+  backButton.addEventListener('click', function(){
+      displayCatagories();
+    });
+  document.getElementById("currentCatagory").appendChild(backButton);
+
+  var br = document.createElement("br");
+  document.getElementById("currentCatagory").appendChild(br);
+}
+
+function createCatagoryButton(catagory){
+  var topic = document.createElement("input");
+  topic.type = "button";
+  topic.value = catagory.topic_name;
+  topic.addEventListener('click', function(){
+
+      displayTopics(catagory);
+    });
+  document.getElementById("currentCatagory").appendChild(topic);
+
+  var br = document.createElement("br");
+  document.getElementById("currentCatagory").appendChild(br);
+}
+
+function createTopicButton(topic){
+  var topicButton = document.createElement("input");
+  topicButton.type = "button";
+  topicButton.value = topic.name;
+  topicButton.addEventListener('click', function(){
+      displayTopicInfo(topic);
+    });
+  document.getElementById("currentCatagory").appendChild(topicButton);
+
+  var br = document.createElement("br");
+  document.getElementById("currentCatagory").appendChild(br);
+}
+
+function displayTopicInfo(topic){
+  if(currentPage != topic.name){
     if(currentPage != null){
       clearCurrentPage("info", "currentPage");
     }
-    printInfo(characterName);
+    printInfo(topic);
   }
 
-  currentPage = characterName;
+  currentPage = topic.name;
 }
 
-function printInfo(characterName){
+function printInfo(data){
   var currentPage = document.createElement("div");
   currentPage.id = "currentPage";
 
   var infoTitle = document.createElement("h1");
   infoTitle.className = "characterTitle";
-  infoTitle.innerHTML = characterName;
+  infoTitle.innerHTML = data.name;
   currentPage.appendChild(infoTitle);
 
   var br = document.createElement("br");
@@ -34,67 +101,70 @@ function printInfo(characterName){
 
   var info = document.createElement("h3");
   info.className = "characterInfo";
-  info.innerHTML = getInfo(characterName);
+  info.innerHTML = data.info;
   currentPage.appendChild(info);
 
   document.getElementById("info").appendChild(currentPage);
-}
-
-function printCatagory(catagoryName){
-  var currentPage = document.createElement("div");
-  currentPage.id = "currentCatagory";
-
-  var infoTitle = document.createElement("h1");
-  infoTitle.className = "characterTitle";
-  infoTitle.innerHTML = catagoryName;
-  currentPage.appendChild(infoTitle);
-
-  var topic1 = document.createElement("input");
-  topic1.type = "button";
-  topic1.value = "CrystalFang"
-  topic1.addEventListener('click', function(){
-      displayCharacter('CrystalFang');
-    });
-  currentPage.appendChild(topic1);
-
-  var topic2 = document.createElement("input");
-  topic2.type = "button";
-  topic2.value = "BurningBrotherhood"
-  topic2.addEventListener('click', function(){
-      displayCharacter('BurningBrotherhood');
-    });
-  currentPage.appendChild(topic2);
-
-  var topic3 = document.createElement("input");
-  topic3.type = "button";
-  topic3.value = "MoonlitShadows"
-  topic3.addEventListener('click', function(){
-      displayCharacter('MoonlitShadows');
-    });
-  currentPage.appendChild(topic3);
-
-  document.getElementById("catagories").appendChild(currentPage);
 }
 
 function clearCurrentPage(parentId, childId){
   document.getElementById(parentId).removeChild(document.getElementById(childId));
 }
 
-function getInfo(characterName){
-  switch (characterName) {
-    case "CrystalFang":
-      return "Yah Boi ";
-      break;
+function getCatagory(catagoryNum){
 
-    case "BurningBrotherhood":
-      return "Big Gay";
-      break;
+  return data[catagoryNum];
+}
 
-    case "MoonlitShadows":
-      return "Small Gay";
-      break;
-
-    default:
-    return "Error";
-  }
+function getData(){
+  let data = `[
+    {
+      "topic_name": "Gangs",
+      "topic": [{
+        "name": "Crystal Fang",
+        "info": "Small Gay"
+        },{
+          "name": "Burning Brotherhood",
+          "info": "Big Gay"
+        },{
+          "name": "Moonlit Shadows",
+          "info": "SUPER MEGA GAY"
+        },{
+          "name": "Black Lotus",
+          "info": "HYPER SUPER GIGA EBOLA GAY"
+        }]
+    },{
+      "topic_name": "Fruits",
+      "topic": [{
+        "name": "Apple",
+        "info": "Applee"
+        },{
+          "name": "Oranga",
+          "info": "Orangee"
+        },{
+          "name": "Pear",
+          "info": "Pearr"
+        },{
+          "name": "Lotus",
+          "info": "Lotuss"
+        }]
+    },{
+      "topic_name": "Charaters",
+      "topic": [{
+        "name": "Maddox",
+        "info": "Dragobborn"
+        },{
+          "name": "Pisci",
+          "info": "Genasi"
+        },{
+          "name": "Arasatra",
+          "info": "Elf"
+        },{
+          "name": "Xerxes",
+          "info": "Half-Elf"
+        }]
+    }
+  ]`
+  
+  return JSON.parse(data);
 }
